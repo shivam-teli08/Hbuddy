f957b5387228febb429d22ad1ac7705737af593a
Shivam Teli <shivamteli63@gmail.com>
2026-03-17 00:11:03 +0530

    .

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from src import create_app
-
+from init_db import db
 
 app = create_app()
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
