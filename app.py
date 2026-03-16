b67cc3d7714e865c72840fd9cd486c590d074bc4
Shivam Teli <shivamteli63@gmail.com>
2026-03-17 00:38:04 +0530

    .

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
+
 from src import create_app
 from src.services.bootstrap_service import initialize_database
 
 
 app = create_app()
 
+with app.app_context():
+    initialize_database()
+    print("Database initialized successfully.")
+
 
 if __name__ == "__main__":
-    with app.app_context():
-        initialize_database()
-        print("Database initialized successfully.")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
